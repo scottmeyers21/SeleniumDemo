694755ca605d687c1000068c8c2c89fc14e7ac0e
Scott Meyers <scottmeyers21@gmail.com>
2018-06-27 10:42:41 -0700

    first commit

---
+++ SeleniumDemo1/AOSHomepageObjects.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumDemo1
+{
+    public class AOSHomepageObjects
+    {
+        IWebDriver driver;
+
+        public AOSHomepageObjects(IWebDriver driver) {
+            this.driver = driver;
+        }
+        public IWebElement UserLink{
+            get
+            {
+                return driver.FindElement(By.Id("menuUser"));
+            }
+        }
+        public IWebElement UserNameField {
+            get {
+                return driver.FindElement(By.Name("username"));
+            }
+        }
+        public IWebElement PasswordField {
+            get {
+                return driver.FindElement(By.Name("password"));
+            }
+        }
+        public IWebElement SignInButton {
+            get {
+                return driver.FindElement(By.Id("sign_in_btnundefined"));
+            }
+        }
+
+
+        public void AOSLogin(string username, string password) {
+            string CurrentWindow = driver.CurrentWindowHandle;
+            UserLink.Click();
+            System.Threading.Thread.Sleep(3000);
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            UserNameField.SendKeys(username);
+            PasswordField.SendKeys(password);
+            SignInButton.Click();
+
+        }
+    }
+}
---
+++ SeleniumDemo1/AOSHomepageTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumDemo1
+{
+    class AOSHomepageTests
+    {
+        IWebDriver driver = new ChromeDriver();
+        [SetUp]
+        public void Initialize() {
+            driver.Navigate().GoToUrl("http://www.advantageonlineshopping.com");
+
+        }
+
+        [Test]
+        public void LoginTest() {
+            AOSHomepageObjects AOSHomepage = new AOSHomepageObjects(driver);
+            AOSHomepage.AOSLogin("scott", "Seahawks21!");
+
+            System.Threading.Thread.Sleep(3000);
+            driver.SwitchTo().Window(driver.WindowHandles.First());
+            AOSHomepage.UserLink.Click();
+            driver.FindElement(By.XPath("//*[@id='loginMiniTitle']/label[1]")).Click();
+
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            string MyAccountText = driver.FindElement(By.TagName("h3")).Text;
+            Console.WriteLine(MyAccountText);
+
+        }
+        [TearDown]
+        public void Close() {
+            driver.Quit();
+
+        }
+    }
+}
